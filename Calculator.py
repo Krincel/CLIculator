e063116dae4cb07b3e8494a637543406011cf307
Krincel <keltonwambu@gmail.com>
2025-02-22 15:54:41 +0000

    Update Calculator.py
    Fixes power issue where it does not give a value when y == 0

--- Calculator.py
+++ Calculator.py
@@ -8,9 +8,7 @@ def divide(x, y):
     if y == 0:
         raise ValueError("Cannot divide by zero!")
     return x / y
-def power(x, y):
-     if y == 0:
-         raise ValueError("Cannot have a number to the power of 0!")
+def power(x, y):
      return x ** y
 
 while True:
